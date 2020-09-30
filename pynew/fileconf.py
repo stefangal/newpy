e93feb3c25d242b1d00384fa64cbc17130de5376
Stefan Gal <stefangal@Stefans-MBP.asusrouter>
2020-09-30 13:49:45 +0200

    Config check and generator works

---
+++ pynew/fileconf.py
@@ -0,0 +1,77 @@
+import os
+import configparser
+
+
+class ConfigGenerator(object):
+    content = {
+        "PROJECT": {
+            "ProjectName": "project",
+            "ProjectPath": "/Users/stefangal/Documents/Coding/Python/Projects",
+        },
+        "GITHUB": {"UserName": "stefangal"},
+        "TODO": {
+            "DocsDirectory": "True",
+            "TestDirecotry": "True",
+            "GithubRepo": "True",
+            "GitPush": "True",
+        },
+        "PYPI": {
+            "Author": "Stefan Gal",
+            "AuthorEmail": "xxx@xxx.com",
+            "Description": "xxx",
+            "Version": "0.0.1",
+            "Release": "False",
+        },
+    }
+
+    def new_config_file(self, path):
+        config = configparser.ConfigParser()
+        with open(os.path.join(path, "config_tmp.ini"), "w") as configfile:
+            for section, values in self.content.items():
+                config[section] = {}
+                for key, value in values.items():
+                    config[section][key] = value
+            config.write(configfile)
+
+    def config_file_test(self, file):
+        file_OK = True
+        config = configparser.ConfigParser()
+        config.read(file)
+
+        print(9 * "_", end="")
+        print("Sections", end="")
+        print(9 * "_")
+        # sections available correctly
+        for section in self.content:
+            # print(section)
+            if not section in config:
+                print(section, "\033[93m \tCorrupt or missing\033[0m".rjust(35))
+                file_OK = False
+                print("\033[91mCannot test further... Please check config file\n")
+                break
+            else:
+                print(section, "\033[92m \t\tOK\033[0m".rjust(20))
+        if not file_OK:
+            return file_OK
+
+        print(11 * "_", end="")
+        print("Keys", end="")
+        print(11 * "_")
+
+        # keys available correctly
+        for section, values in self.content.items():
+            for value in values:
+                if value not in config[section]:
+                    print(value.strip(), "\033[93m \tCorrupt or missing\033[0m")
+                    file_OK = False
+                else:
+                    print(value.strip(), "\033[92m \tOK\033[0m".rjust(21))
+
+        print(file_OK)
+        return file_OK
+
+
+# if __name__ == "__main__":
+#     run = ConfigGenerator()
+#     # run.new_config_file('.')
+#     run.config_file_test("config_tmp.ini", "config.ini")
--- pynew/main.py
+++ pynew/main.py
@@ -1,25 +1,39 @@
 import os
+import logging
 import configparser
-
+import fileconf as cg
 
 
 MAJOR = 0
 MINOR = 91
 MICRO = 0
+__version__ = f"{MAJOR}.{MINOR}.{MICRO}"
 
-__version__ = f'{MAJOR}.{MINOR}.{MICRO}'
+logger = logging.getLogger("new_project")
+logger.setLevel(logging.DEBUG)
 
 
 class Run:
 
-    CONFIG_FILE = 'config.ini'
+    CONFIG_FILE = "config.ini"
 
     def __init__(self):
+        # Logger
+        formatter = logging.Formatter(
+            "%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s"
+        )
+        self.logger = logging.getLogger("new_project")
+        self.log = logging.StreamHandler()
+        self.log.setFormatter(formatter)
+        self.logger.addHandler(self.log)
+        # Configparser
         self.config = configparser.ConfigParser()
-        
-    def project_details(self):
         self.config.read(self.CONFIG_FILE)
-        print(self.config['GITHUB']['UserName'])
+        # Methods to run
+        self.build_folder_structure()
+
+    def project_details(self):
+        print(self.config["GITHUB"]["UserName"])
 
     def github(self):
         pass
@@ -28,11 +42,43 @@ class Run:
 
         pass
 
-    def build_structure(self):
+    def config_file_test(self):
+        if self.CONFIG_FILE != "config.ini":
+            return False
+
+    def build_folder_structure(self):
+        check = cg.ConfigGenerator()
+        if check.config_file_test("config.ini"):
+            print("all OK ")
+        PATH = self.config["PROJECT"]["ProjectPath"]
+        FILE = self.config["PROJECT"]["ProjectName"]
+        filepath = os.path.join(PATH, FILE)
+
+        if PATH and FILE and not os.path.exists(filepath):
+            os.mkdir(path=os.path.join(PATH, FILE))
+            os.mkdir(path=os.path.join(PATH, "tests"))
+            os.mkdir(path=os.path.join(PATH, "docs"))
+            self.logger.info("Folder structure ready")
+
+        if os.path.exists(filepath):
+            with open(os.path.join(filepath, "main.py"), "w") as file:
+                pass
+            with open(os.path.join(PATH, ".gitignore"), "w") as file:
+                pass
+            with open(os.path.join(PATH, ".LICENSE"), "w") as file:
+                pass
+            with open(os.path.join(PATH, "requirements.txt"), "w") as file:
+                pass
+            with open(os.path.join(PATH, "setup.py"), "w") as file:
+                pass
+            with open(os.path.join(PATH, "README.MD"), "w") as file:
+                pass
+        return True
+
+    def build_file_structure(self):
         pass
 
-    def start(self):
-        self.project_details()
+
 if __name__ == "__main__":
     run = Run()
-    run.start()
+
