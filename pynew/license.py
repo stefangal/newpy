65d181ad94438c46188b487d31a8f01251dff866
StefPy <stefan.mail.sk@gmail.com>
2020-10-07 15:20:33 +0200

    Newlicense changes

--- pynew/license.py
+++ pynew/license.py
@@ -5,8 +5,13 @@
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
 import os
+from os import CLD_EXITED, linesep
 import shutil
+import lice
+import lice.core as lc
+import sys
 
+# from license.manager import LicenseManager
 
 class NewLicense:
     def download_license(self, path, lictype):
@@ -23,17 +28,37 @@ class NewLicense:
         Generate from templace and download to the path.
         """
 
-        FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)) + "/templates")
-
+        FOLDER = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)) + "/templates")
+        print(FOLDER)
         if os.path.exists(FOLDER):
             for template_file in os.listdir(FOLDER):
                 if lictype in template_file:
                     shutil.copy(os.path.join(FOLDER, template_file), dst)
-                    os.rename(os.path.join(dst, template_file), "LICENSE")
+                    os.rename(os.path.join(dst, template_file),
+                              os.path.join(dst, "LICENSE"))
 
+    def save(self, dst, lic_type):
+        """
+        Generate from templace and download to the path.
+        """
+
+        # lc.DEFAULT_LICENSE = lic_type
+
+        # if os.path.exists(dst):
+        #     with open(os.path.join(dst, "LICENSE"), "w") as license_file:
+        #         print(lc.main().__str__())
+        #         for line in lc.main():
+        #             license_file.writelines(str(line))
+
+        if os.path.exists(dst):
+            manager = LicenseManager(dst)
+            manager.get('mit')
 
 if __name__ == "__main__":
-    license = NewLicense()
+    lic = NewLicense()
 
-    license.download(
-        "/Users/stefangal/Documents/Coding/Python/Projects/PyNew/DIR", 'agpl3')
+    # license.download(
+    #     "/Users/stefangal/Documents/Coding/Python/Projects/PyNew/DIR", 'agpl3')
+    lic.save("/Users/stefangal/Documents/Coding/Python/Projects/PyNew/DIR",
+                 'mit')
