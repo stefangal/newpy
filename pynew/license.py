bdb76e6c8d6d75b9583d2e5897c207a1562fe508
StefPy <stefan.mail.sk@gmail.com>
2020-10-08 12:59:01 +0200

    License downloader ready

--- pynew/license.py
+++ pynew/license.py
@@ -5,35 +5,33 @@
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
 import os
-import shutil
+
+import subprocess as sp
 
 
 class NewLicense:
-    def download_license(self, path, lictype):
+    """
+    To get the required LICENSE file.
+    Credit to Jeremy Carbaugh for the 'lice' the License file genarator: https://github.com/licenses/lice
+    """
+    def download(self, dst, lictype):
         """
-        Choose from: 
+        Generating LICENSE file. Some data are gathered from git config.
+
+        Choose license type from: 
          'afl3', 'agpl3', 'apache', 'bsd2', 'bsd3', 'cc0', 'cc_by', 'cc_by_nc', 
          'cc_by_nc_nd', 'cc_by_nc_sa', 'cc_by_nd', 'cc_by_sa', 'cddl', 'epl', 
          'gpl2', 'gpl3', 'isc', 'lgpl', 'mit', 'mpl', 'wtfpl', 'zlib'
         """
-        os.system(f"lice {lictype} -f {path}/LICENSE")
-
-    def download(self, dst, lictype):
-        """
-        Generate from templace and download to the path.
-        """
-
-        FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)) + "/templates")
-
+        FOLDER = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)) + "/templates")
         if os.path.exists(FOLDER):
-            for template_file in os.listdir(FOLDER):
-                if lictype in template_file:
-                    shutil.copy(os.path.join(FOLDER, template_file), dst)
-                    os.rename(os.path.join(dst, template_file), "LICENSE")
+            sp.Popen(f"lice {lictype} -f {dst}/LICENSE", shell=True).wait()
+            print(f"Download of {lictype} license successful!")
+            print(f"LICENSE save in folder: {dst}\n")
 
 
 if __name__ == "__main__":
     license = NewLicense()
-
     license.download(
-        "/Users/stefangal/Documents/Coding/Python/Projects/PyNew/DIR", 'agpl3')
+        "/Users/stefangal/Documents/Coding/Python/Projects/PyNew/DIR", 'mit')
